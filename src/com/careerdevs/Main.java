60b30e346368df7bdb78fc5d2fb22276cd98c3c5
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-31 20:15:24 -0500

    Initial commit ui

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,20 @@
+package com.careerdevs;
+
+public class Main {
+
+        public static void main(String[] args) {
+            // write your code here
+            System.out.println("Welcome to the Algorithn CLI");
+            mainMenu();
+
+        }
+
+        private static void mainMenu() {
+           // System.out.println(UI.lineSpacer() + "\n\t\t MAIN MENU\n" + UI.lineSpacer);
+            System.out.println("1) Repeat A String");
+            System.out.println("2) Exit The Program");
+
+        }
+
+    }
+
