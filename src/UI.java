e4470a166b2d84c07c37621c13a01cf29b99ecd9
961026bm <olubunmiosunde.student@careerdevs.com>
2022-02-12 19:55:35 -0500

    update to algorithm CLI

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,42 @@
-public class UI {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class UI {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String readString(String question) {
+        System.out.println(question + "\nInput: ");
+        return scanner.nextLine();
+    }
+
+    public static int readInt (String question, int min, int max) {
+        while (true) {
+            try {
+                System.out.println(question);
+                System.out.print("Number (" + min + " - " + max + "): ");
+
+                int userInt = scanner.nextInt(); //checking for type exception risk
+
+                // make sure to clear System.in using the line after taking in any data that is not a string
+
+                if (userInt >= min && userInt <= max ) { // checking for wild range
+                     return userInt;
+            }
+                // these lines will not run if a valid input is given
+                System.out.println("Number must be in the range (" + min + " ' " + max + ")" );
+
+        } catch (InputMismatchException e) {
+                System.out.println("You must enter a whole number, try again... ");
+                scanner.nextLine();
+
+            } catch (exception e) {
+                System.out.println("An error unknown occurred");
+                System.out.println(e + "\n" + e.getMessage());
+
+            }
+
+        }
+    }
 }
+
+
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -2,19 +2,11 @@ package com.careerdevs;
 
 public class Main {
 
-        public static void main(String[] args) {
-            // write your code here
-            System.out.println("Welcome to the Algorithn CLI");
-            mainMenu();
-
-        }
-
-        private static void mainMenu() {
-           // System.out.println(UI.lineSpacer() + "\n\t\t MAIN MENU\n" + UI.lineSpacer);
-            System.out.println("1) Repeat A String");
-            System.out.println("2) Exit The Program");
-
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Algorithm CLI");
+    }
+    private static void mainMenu() {
 
     }
+}
 
